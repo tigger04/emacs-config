e35cf5720080a71f8dec48d0325ed31328eebdd5
tecosaur <20903656+tecosaur@users.noreply.github.com>
2020-01-10 01:51:54 +0800

    move custom file

--- init.el
+++ init.el
@@ -183,17 +183,3 @@
        literate
        (default +bindings +smartparens))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("6cbf6003e137485fb3f904e76fb15bc48abc386540f43f54e2a47a9884e679f6" "423435c7b0e6c0942f16519fa9e17793da940184a50201a4d932eafe4c94c92d" "e47c0abe03e0484ddadf2ae57d32b0f29f0b2ddfe7ec810bd6d558765d9a6a6c" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
