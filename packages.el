d6c9330f01bb1be8857d2d3b7df4444ca0e13b9b
tecosaur <20903656+tecosaur@users.noreply.github.com>
2020-02-03 13:04:56 +0800

    Refresh tangles .el config files

--- packages.el
+++ packages.el
@@ -1,10 +1,38 @@
 ;; -*- no-byte-compile: t; -*-
+
 (package! company-tabnine ; tab9 autocomplete
   :recipe (:host github :repo "TommyX12/company-tabnine"
            :files ("company-tabnine.el" "fetch-binaries.sh")))
+
 (package! prettify-utils ; simplify messing with prettify-mode
   :recipe (:host github :repo "Ilazki/prettify-utils.el"))
+
 (package! org-pretty-table-mode
   :recipe (:host github :repo "Fuco1/org-pretty-table"))
+
 (package! ox-gfm)
+
 (package! ox-pandoc)
+
+(package! rotate)
+
+(package! xkcd)
+
+(package! selectric-mode)
+
+(package! wttrin)
+
+(package! spray)
+
+(package! theme-magic)
+
+(use-package! beancount :load-path "~/.config/doom/lisp")
+
+(package! flyspell-lazy)
+
+(package! calctex :recipe (:host github :repo "johnbcoughlin/calctex"
+                                 :files ("*.el")))
+
+(package! systemd)
+
+(package! ess-view)
