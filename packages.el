d761ef928f46ec9bdfbb1ef9df8252c022f4acdc
tecosaur <20903656+tecosaur@users.noreply.github.com>
2020-04-10 02:30:02 +0800

    Update generated files

--- packages.el
+++ packages.el
@@ -5,37 +5,41 @@
 ;; [[file:~/.config/doom/config.org::*Auto-complete][Auto-complete:1]]
 (package! company-tabnine ; tab9 autocomplete
   :recipe (:host github :repo "TommyX12/company-tabnine"
-           :files ("company-tabnine.el" "fetch-binaries.sh")))
+           :files ("company-tabnine.el" "fetch-binaries.sh")) :pin "e986a4ad0d")
 ;; Auto-complete:1 ends here
 
 ;; [[file:~/.config/doom/config.org::*Prettification][Prettification:1]]
 (package! prettify-utils ; simplify messing with prettify-mode
-  :recipe (:host github :repo "Ilazki/prettify-utils.el"))
+  :recipe (:host github :repo "Ilazki/prettify-utils.el") :pin "8b783d316c")
 ;; Prettification:1 ends here
 
 ;; [[file:~/.config/doom/config.org::*Window management][Window management:1]]
-(package! rotate)
+(package! rotate :pin "091b5ac4fc")
 ;; Window management:1 ends here
 
 ;; [[file:~/.config/doom/config.org::*Fun][Fun:1]]
-(package! xkcd)
+(package! xkcd :pin "66e928706f")
 ;; Fun:1 ends here
 
 ;; [[file:~/.config/doom/config.org::*Fun][Fun:2]]
-(package! selectric-mode)
+(package! selectric-mode :pin "bb9e66678f")
 ;; Fun:2 ends here
 
 ;; [[file:~/.config/doom/config.org::*Fun][Fun:3]]
-(package! wttrin)
+(package! wttrin :pin "df5427ce2a")
 ;; Fun:3 ends here
 
 ;; [[file:~/.config/doom/config.org::*Fun][Fun:4]]
-(package! spray)
+(package! spray :pin "00638bc916")
 ;; Fun:4 ends here
 
 ;; [[file:~/.config/doom/config.org::*Fun][Fun:5]]
-(package! theme-magic)
+(package! theme-magic :pin "844c4311bd")
 ;; Fun:5 ends here
+
+;; [[file:~/.config/doom/config.org::*Fun][Fun:6]]
+(package! elcord :pin "94b0afb9ba")
+;; Fun:6 ends here
 
 ;; [[file:~/.config/doom/config.org::*Flyspell-lazy][Flyspell-lazy:1]]
 (package! flyspell-lazy)
@@ -43,38 +47,47 @@
 
 ;; [[file:~/.config/doom/config.org::*CalcTeX][CalcTeX:1]]
 (package! calctex :recipe (:host github :repo "johnbcoughlin/calctex"
-                                 :files ("*.el")))
+                                 :files ("*.el")) :pin "542b628eb4")
 ;; CalcTeX:1 ends here
 
 ;; [[file:~/.config/doom/config.org::*ESS][ESS:1]]
-(package! ess-view)
+(package! ess-view :pin "d4e5a340b7")
 ;; ESS:1 ends here
 
 ;; [[file:~/.config/doom/config.org::*Systemd][Systemd:1]]
-(package! systemd)
+(package! systemd :pin "51c148e09a")
 ;; Systemd:1 ends here
 
 ;; [[file:~/.config/doom/config.org::*Org Mode][Org Mode:1]]
 (package! org-pretty-table-mode
-  :recipe (:host github :repo "Fuco1/org-pretty-table"))
+  :recipe (:host github :repo "Fuco1/org-pretty-table") :pin "88380f865a")
 ;; Org Mode:1 ends here
 
 ;; [[file:~/.config/doom/config.org::*Org Mode][Org Mode:2]]
-(package! ox-gfm)
+(package! ox-gfm :pin "99f93011b0")
 ;; Org Mode:2 ends here
 
 ;; [[file:~/.config/doom/config.org::*Org Mode][Org Mode:3]]
-(package! org-ref)
+(package! org-ref :pin "5bb9be2232")
 ;; Org Mode:3 ends here
 
 ;; [[file:~/.config/doom/config.org::*Org Mode][Org Mode:4]]
-(package! org-fragtog)
+(package! org-fragtog :pin "8eca8084cc")
 ;; Org Mode:4 ends here
 
 ;; [[file:~/.config/doom/config.org::*Org Mode][Org Mode:5]]
-(package! org-graph-view :recipe (:host github :repo "alphapapa/org-graph-view"))
+(package! org-graph-view :recipe (:host github :repo "alphapapa/org-graph-view") :pin "13314338d7")
 ;; Org Mode:5 ends here
 
 ;; [[file:~/.config/doom/config.org::*Org Mode][Org Mode:6]]
-(package! org-chef)
+(package! org-chef :pin "67f50aa2a2")
 ;; Org Mode:6 ends here
+
+;; [[file:~/.config/doom/config.org::*Org Mode][Org Mode:7]]
+(package! org-bullets :disable t :pin "767f55feb5")
+(package! org-superstar
+  :recipe (:host github :repo "integral-dw/org-superstar-mode")
+  :pin "4659cfc120")
+(package! org-fancy-priorities :pin "819bb993b7")
+(package! org-pretty-tags :pin "40fd72f3e7")
+;; Org Mode:7 ends here
