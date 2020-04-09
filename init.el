8ac72714d7c3f3aa2cb31c6864dd0ccb69013884
tecosaur <20903656+tecosaur@users.noreply.github.com>
2020-04-10 02:29:06 +0800

    Stop using centuar-tabs (for now)

--- init.el
+++ init.el
@@ -44,8 +44,8 @@
         +all             ; catch all popups that start with an asterix
         +defaults)       ; default popup rules
        pretty-code       ; replace bits of code with pretty symbols
-       (tabs             ; an tab bar for Emacs
-         +centaur-tabs)  ; ... with prettier tabs
+       ;; (tabs             ; an tab bar for Emacs
+       ;;   +centaur-tabs)  ; ... with prettier tabs
        treemacs          ; a project drawer, like neotree but cooler
        ;;unicode           ; extended unicode support for various languages
        vc-gutter         ; vcs diff in the fringe
